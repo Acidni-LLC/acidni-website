74ceb5a6ac8e7550b58febae4612f6639d86f044
savitas1 <jgill@savitas.net>
2026-01-07 13:21:22 +0000

    feat(analytics): add Google Tag Manager (GTM-WW8F3D6T)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -158,6 +158,16 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WW8F3D6T');
+          `}
+        </Script>
         {/* Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-G1DE0HJTKF"
@@ -186,6 +196,15 @@ export default function RootLayout({
         </Script>
       </head>
       <body className="min-h-screen bg-slate-900">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WW8F3D6T"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <Navbar />
         <main>{children}</main>
         <Footer />
